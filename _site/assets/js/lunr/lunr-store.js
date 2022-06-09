30808af29073206c78b70ea6e7b16f986eb2b0d2
bo-oseng <ses7720@gmail.com>
2022-06-09 22:32:45 +0900

    move old_blog_posts to new_blog

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "백준 문제 중 15685번, 구현, 드래곤커브",
+        "excerpt":"백준 문제 중 15685번 https://www.acmicpc.net/problem/15685 문제 드래곤 커브는 다음과 같은 세 가지 속성으로 이루어져 있으며, 이차원 좌표 평면 위에서 정의된다. 좌표 평면의 x축은 → 방향, y축은 ↓ 방향이다. 시작 점 시작 방향 세대 0세대 드래곤 커브는 아래 그림과 같은 길이가 1인 선분이다. 아래 그림은 (0, 0)에서 시작하고, 시작 방향은 오른쪽인...","categories": ["BOJ"],
+        "tags": ["python","백준온라인저지","코딩테스트"],
+        "url": "/boj/boj-15685/",
+        "teaser": null
+      },{
+        "title": "Programmers, 구현, Summer/Winter Coding(2019) 중 '멀쩡한 사각형'",
+        "excerpt":"프로그래머스 문제중 6208번 ‘멀쩡한 사각형’ https://programmers.co.kr/learn/courses/30/lessons/60057 문제 설명 가로 길이가 Wcm, 세로 길이가 Hcm인 직사각형 종이가 있습니다. 종이에는 가로, 세로 방향과 평행하게 격자 형태로 선이 그어져 있으며, 모든 격자칸은 1cm x 1cm 크기입니다. 이 종이를 격자 선을 따라 1cm × 1cm의 정사각형으로 잘라 사용할 예정이었는데, 누군가가 이 종이를 대각선 꼭지점...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","구현"],
+        "url": "/programmers/programmers-62048/",
+        "teaser": null
+      },{
+        "title": "Programmers, 구현, 2019 KAKAO BLIND RECRUITMENT 중 '오픈채팅방'",
+        "excerpt":"프로그래머스 문제 중 42888번 오픈채팅방 https://programmers.co.kr/learn/courses/30/lessons/42888 문제 설명 카카오톡 오픈채팅방에서는 친구가 아닌 사람들과 대화를 할 수 있는데, 본래 닉네임이 아닌 가상의 닉네임을 사용하여 채팅방에 들어갈 수 있다. 신입사원인 김크루는 카카오톡 오픈 채팅방을 개설한 사람을 위해, 다양한 사람들이 들어오고, 나가는 것을 지켜볼 수 있는 관리자창을 만들기로 했다. 채팅방에 누군가 들어오면 다음...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","구현"],
+        "url": "/programmers/programmers-42888/",
+        "teaser": null
+      },{
+        "title": "Programmers, 문자열, 2020 KAKAO BLIND RECRUITMENT 중 '문자열 압축'",
+        "excerpt":"프로그래머스 문제 중 60057번 ‘문자열 압축’ https://programmers.co.kr/learn/courses/30/lessons/60057 문제 설명 데이터 처리 전문가가 되고 싶은 “어피치”는 문자열을 압축하는 방법에 대해 공부를 하고 있습니다. 최근에 대량의 데이터 처리를 위한 간단한 비손실 압축 방법에 대해 공부를 하고 있는데, 문자열에서 같은 값이 연속해서 나타나는 것을 그 문자의 개수와 반복되는 값으로 표현하여 더 짧은 문자열로...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","문자열"],
+        "url": "/programmers/programmers-60057/",
+        "teaser": null
+      },{
+        "title": "Programmers, 구현, 연습문제 중 '124 나라의 숫자'",
+        "excerpt":"프로그래머스 문제 중 12899번 ‘124 나라의 숫자’ https://programmers.co.kr/learn/courses/30/lessons/12899 문제 설명 124 나라가 있습니다. 124 나라에서는 10진법이 아닌 다음과 같은 자신들만의 규칙으로 수를 표현합니다. 124 나라에는 자연수만 존재합니다. 124 나라에는 모든 수를 표현할 때 1, 2, 4만 사용합니다. 예를 들어서 124 나라에서 사용하는 숫자는 다음과 같이 변환됩니다. 10진법 123 나라 10진법...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","구현"],
+        "url": "/programmers/programmers-12899/",
+        "teaser": null
+      },{
+        "title": "Programmers, 스택/큐, '기능개발'",
+        "excerpt":"프로그래머스 문제 중 42586번 ‘기능개발’ https://programmers.co.kr/learn/courses/30/lessons/42586?language=python3 문제 설명 프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다. 또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","스택/큐"],
+        "url": "/programmers/programmers-42586/",
+        "teaser": null
+      },{
+        "title": "Programmers, 백트레킹, '타겟넘버'",
+        "excerpt":"프로그래머스 문제 중 43165번 ‘타겟넘버’ https://programmers.co.kr/learn/courses/30/lessons/43165 문제설명 n개의 음이 아닌 정수들이 있습니다. 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다. -1+1+1+1+1 = 3 +1-1+1+1+1 = 3 +1+1-1+1+1 = 3 +1+1+1-1+1...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","백트래킹"],
+        "url": "/programmers/programmers-43165/",
+        "teaser": null
+      },{
+        "title": "Programmers, 힙(heap), '더 맵게'",
+        "excerpt":"프로그래머스 문제 중 42626번 ‘더 맵게’ https://programmers.co.kr/learn/courses/30/lessons/42626 문제설명 매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다. 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다. 섞은 음식의 스코빌 지수 = 가장...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","힙(heap)"],
+        "url": "/programmers/programmers-42626/",
+        "teaser": null
+      },{
+        "title": "Programmers, 스택, 2017 팁스타운 중 '짝지어 제거하기'",
+        "excerpt":"프로그래머스 문제 중 12973번 ‘짝지어 제거하기’ https://programmers.co.kr/learn/courses/30/lessons/12973 문제설명 짝지어 제거하기는, 알파벳 소문자로 이루어진 문자열을 가지고 시작합니다. 먼저 문자열에서 같은 알파벳이 2개 붙어 있는 짝을 찾습니다. 그다음, 그 둘을 제거한 뒤, 앞뒤로 문자열을 이어 붙입니다. 이 과정을 반복해서 문자열을 모두 제거한다면 짝지어 제거하기가 종료됩니다. 문자열 S가 주어졌을 때, 짝지어 제거하기를 성공적으로...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","스택"],
+        "url": "/programmers/programmers-12973/",
+        "teaser": null
+      },{
+        "title": "Programmers, 2021 Dev-Matching 중, 구현, '행렬 테두리 회전'",
+        "excerpt":"프로그래머스 문제 중 77485번 ‘행렬 테두리 회전’ https://programmers.co.kr/learn/courses/30/lessons/77485 rows x columns 크기인 행렬이 있습니다. 행렬에는 1부터 rows x columns까지의 숫자가 한 줄씩 순서대로 적혀있습니다. 이 행렬에서 직사각형 모양의 범위를 여러 번 선택해, 테두리 부분에 있는 숫자들을 시계방향으로 회전시키려 합니다. 각 회전은 (x1, y1, x2, y2)인 정수 4개로 표현하며, 그 의미는...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","구현"],
+        "url": "/programmers/programmers-77485/",
+        "teaser": null
+      },{
+        "title": "Programmers, 2021 KAKAO BLIND RECRUITMENT 중, 구현, '메뉴 리뉴얼'",
+        "excerpt":"프로그래머스 문제 중 72411번 ‘메뉴 리뉴얼’ https://programmers.co.kr/learn/courses/30/lessons/72411 문제 설명 레스토랑을 운영하던 스카피는 코로나19로 인한 불경기를 극복하고자 메뉴를 새로 구성하려고 고민하고 있습니다. 기존에는 단품으로만 제공하던 메뉴를 조합해서 코스요리 형태로 재구성해서 새로운 메뉴를 제공하기로 결정했습니다. 어떤 단품메뉴들을 조합해서 코스요리 메뉴로 구성하면 좋을 지 고민하던 “스카피”는 이전에 각 손님들이 주문할 때 가장 많이...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","구현"],
+        "url": "/programmers/programmers-72411/",
+        "teaser": null
+      },{
+        "title": "Programmers, 2020 KAKAO BLIND RECRUITMENT 중, 스택, 재귀,'괄호 변환'",
+        "excerpt":"프로그래머스 문제 중 60058번 괄호변환 https://programmers.co.kr/learn/courses/30/lessons/60058 문제설명 카카오에 신입 개발자로 입사한 “콘”은 선배 개발자로부터 개발역량 강화를 위해 다른 개발자가 작성한 소스 코드를 분석하여 문제점을 발견하고 수정하라는 업무 과제를 받았습니다. 소스를 컴파일하여 로그를 보니 대부분 소스 코드 내 작성된 괄호가 개수는 맞지만 짝이 맞지 않은 형태로 작성되어 오류가 나는 것을 알게...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","스택","재귀"],
+        "url": "/programmers/programmers-60058/",
+        "teaser": null
+      },{
+        "title": "Programmers, 2018 KAKAO BLIND RECRUITMENT 중, 구현, '뉴스 클러스터링'",
+        "excerpt":"문제설명 뉴스 클러스터링 여러 언론사에서 쏟아지는 뉴스, 특히 속보성 뉴스를 보면 비슷비슷한 제목의 기사가 많아 정작 필요한 기사를 찾기가 어렵다. Daum 뉴스의 개발 업무를 맡게 된 신입사원 튜브는 사용자들이 편리하게 다양한 뉴스를 찾아볼 수 있도록 문제점을 개선하는 업무를 맡게 되었다. 개발의 방향을 잡기 위해 튜브는 우선 최근 화제가 되고 있는...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","구현"],
+        "url": "/programmers/programmers-17677/",
+        "teaser": null
+      },{
+        "title": "Programmers, 2021 카카오 채용 연계형 인턴쉽, 완전탐색, DFS/BFS '거리두기 확인'",
+        "excerpt":"프로그래머스 문제 중 81302번 ‘거리두기 확인하기’ https://programmers.co.kr/learn/courses/30/lessons/81302 문제설명 개발자를 희망하는 죠르디가 카카오에 면접을 보러 왔습니다. 코로나 바이러스 감염 예방을 위해 응시자들은 거리를 둬서 대기를 해야하는데 개발 직군 면접인 만큼 아래와 같은 규칙으로 대기실에 거리를 두고 앉도록 안내하고 있습니다. 대기실은 5개이며, 각 대기실은 5x5 크기입니다. 거리두기를 위하여 응시자들 끼리는 맨해튼 거리1가...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","완전탐색","DFS/BFS"],
+        "url": "/programmers/programmers-81302/",
+        "teaser": null
+      },{
+        "title": "Programmers, 2020 카카오 인턴쉽, 스택, '수식 최대화'",
+        "excerpt":"프로그래머스 문제 중 67257번 ‘수식 최대화’ https://programmers.co.kr/learn/courses/30/lessons/67257 문제설명 IT 벤처 회사를 운영하고 있는 라이언은 매년 사내 해커톤 대회를 개최하여 우승자에게 상금을 지급하고 있습니다. 이번 대회에서는 우승자에게 지급되는 상금을 이전 대회와는 다르게 다음과 같은 방식으로 결정하려고 합니다. 해커톤 대회에 참가하는 모든 참가자들에게는 숫자들과 3가지의 연산문자(+, -, ) 만으로 이루어진 연산 수식이...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","스택"],
+        "url": "/programmers/programmers-67257/",
+        "teaser": null
+      },{
+        "title": "Programmers, 월간 코드 챌린지 시즌3, 구현, DFS/BFS, '빛의 경로 사이클'",
+        "excerpt":"프로그래머스 문제 중 86052번 ‘빛의 경로 사이클’ https://programmers.co.kr/learn/courses/30/lessons/86052 문제설명 각 칸마다 S, L, 또는 R가 써져 있는 격자가 있습니다. 당신은 이 격자에서 빛을 쏘고자 합니다. 이 격자의 각 칸에는 다음과 같은 특이한 성질이 있습니다. 빛이 “S”가 써진 칸에 도달한 경우, 직진합니다. 빛이 “L”이 써진 칸에 도달한 경우, 좌회전을 합니다. 빛이...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","구현","DFS/BFS"],
+        "url": "/programmers/programmer-86052/",
+        "teaser": null
+      },{
+        "title": "Programmers, 코딩테스트 연습, 해시, '전화번호 목록'",
+        "excerpt":"프로그래머스 문제 중 42577번 ‘전화번호 목록’ https://programmers.co.kr/learn/courses/30/lessons/42577 문제설명 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다. 전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다. 구조대 : 119 박준영 : 97 674 223 지영석 : 11 9552 4421 전화번호부에 적힌 전화번호를 담은 배열 phone_book 이...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","해시"],
+        "url": "/programmers/programmer-42577/",
+        "teaser": null
+      },{
+        "title": "Programmers, 코딩테스트 연습, 스택/큐, '가장 큰 수'",
+        "excerpt":"프로그래머스 문제 중 42746번 ‘가장 큰 수’ https://programmers.co.kr/learn/courses/30/lessons/42746 문제 설명 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요. 예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다. 0 또는...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","스택","functools.cmp_to_key"],
+        "url": "/programmers/programmer-42746/",
+        "teaser": null
+      },{
+        "title": "API authentication by API key, API key로 API 인증하기",
+        "excerpt":"API key access 배경 최근 Vanila Javascript 예제를 몇개 만들어 보고 있는데, Exachange Rate Calculator 에서 API를 다룬적이 있습니다. API를 써본적은 있었지만 API로의 접근에서 권한을 필요로 하는 API는 처음 만나게 되었습니다. API를 제공하는 사이트의 스펙에서 사용법과 API key를 친절히 제공해 주어 쉽게 다룰 수 있었지만 어떤 원리로 권한을 인가받고 API...","categories": ["HTTP"],
+        "tags": ["authentication","authorization","headers","Web"],
+        "url": "/http/web-api_key_access/",
+        "teaser": null
+      },{
+        "title": "Programmers, 코딩테스트 연습, 완전탐색,  '소수찾기'",
+        "excerpt":"프로그래머스 문제 중 ‘소수찾기’ 문제 설명 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다. 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요. 제한사항 numbers는 길이...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","완전탐색"],
+        "url": "/programmers/programmers-42839/",
+        "teaser": null
+      },{
+        "title": "Programmers, 찾아라 프로그래밍 마에스터, 완전탐색, '게임 맵 최단거리'",
+        "excerpt":"프로그래머스 문제 중 ‘게임 맵 최단거리’ 문제설명 ROR 게임은 두 팀으로 나누어서 진행하며, 상대 팀 진영을 먼저 파괴하면 이기는 게임입니다. 따라서, 각 팀은 상대 팀 진영에 최대한 빨리 도착하는 것이 유리합니다. 지금부터 당신은 한 팀의 팀원이 되어 게임을 진행하려고 합니다. 다음은 5 x 5 크기의 맵에, 당신의 캐릭터가 (행: 1,...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","완전탐색"],
+        "url": "/programmers/programmers-1844/",
+        "teaser": null
+      },{
+        "title": "Programmers, 코딩테스트 연습, 그리디,  '조이스틱'",
+        "excerpt":"프로그래머스 문제 중 ‘조이스틱’ 문제설명 조이스틱으로 알파벳 이름을 완성하세요. 맨 처음엔 A로만 이루어져 있습니다. ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA 조이스틱을 각 방향으로 움직이면 아래와 같습니다. ▲ - 다음 알파벳 ▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로) ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서...","categories": ["Programmers"],
+        "tags": ["python","프로그래머스","코딩테스트","그리디"],
+        "url": "/programmers/programmers-42860/",
+        "teaser": null
+      }]
